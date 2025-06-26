5358e0e71bd5c2697f29a6251d7a02eeefebe742
Alisson <jameson2good@gmail.com>
2025-06-26 08:34:32 +0100

    updated

---
+++ about/about.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", () => {
+  document.getElementById("hamburger").classList.toggle("toggle");
+  document.getElementById("nav-links").classList.toggle("show");
+});
